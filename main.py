4bd605b09034184e9d4cbd37101ea97c29f2ec4d
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2023-05-05 22:55:40 -0400

    fixed the timeout error

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ async def init(ctx):
                 except IntegrityError:
                     session.rollback()
                 session.close()
-            await ctx.send("Database initialized")
+            await ctx.send_response("Database initialized")
     logging.debug(f"{ctx.author.name} initialized the database")
 
     if admin_found is False:
-        await ctx.send("You don't have permission to do that")
+        await ctx.send_response("You don't have permission to do that")
 
 
 @bot.slash_command(name="vip", description="Add a member to the VIP list")
@@ -97,13 +97,13 @@ async def vip(ctx, member: discord.Member, enable: bool):
             member.is_vip = enable
             session.commit()
             if enable:
-                await ctx.send(f"{member.discord_username} added to VIP list")
+                await ctx.send_response(f"{member.discord_username} added to VIP list")
             else:
-                await ctx.send(f"{member.discord_username} removed from VIP list")
+                await ctx.send_response(f"{member.discord_username} removed from VIP list")
             logging.debug(f"{ctx.author.name} added {member.discord_username} to VIP list")
             session.close()
     if admin_found is False:
-        await ctx.send("You don't have permission to do that")
+        await ctx.send_response("You don't have permission to do that")
 
 
 @bot.slash_command(name="resumecv", description="Add a member to the resume.cv list")
@@ -117,13 +117,13 @@ async def resumecv(ctx, member: discord.Member, enable: bool):
             member.is_resumecv = enable
             session.commit()
             if enable:
-                await ctx.send(f"{member.discord_username} added to resume.cv list")
+                await ctx.send_response(f"{member.discord_username} added to resume.cv list")
             else:
-                await ctx.send(f"{member.discord_username} removed from resume.cv list")
+                await ctx.send_response(f"{member.discord_username} removed from resume.cv list")
             logging.debug(f"{ctx.author.name} added {member.discord_username} to resume.cv list")
             session.close()
     if admin_found is False:
-        await ctx.send("You don't have permission to do that")
+        await ctx.send_response("You don't have permission to do that")
 
 
 @bot.slash_command(name="waitlist", description="Check your placement on the waitlist", ephemeral=True)
@@ -148,10 +148,24 @@ async def waitlist(ctx):
     session.close()
 
 
-@bot.slash_command(name="test", description="Test command")
-@commands.guild_only()
-async def test(ctx):
-    ctx.send(ctx.author.message_count)
+@bot.slash_command(name="invite", description="Set user as invited to bluesky")
+async def invite(ctx, member: discord.Member, enable: bool):
+    admin_found = False
+    for role in ctx.author.roles:
+        if role.id == ROLE:
+            admin_found = True
+            session = Session()
+            member = session.query(Member).filter(Member.discord_id == member.id).first()
+            member.is_invited = enable
+            session.commit()
+            if enable:
+                await ctx.send_response(f"{member.discord_username} added to invited list")
+            else:
+                await ctx.send_response(f"{member.discord_username} removed from invited list")
+            logging.debug(f"{ctx.author.name} added {member.discord_username} to invited list")
+            session.close()
+    if admin_found is False:
+        await ctx.send_response("You don't have permission to do that")
 
 
 @bot.event
